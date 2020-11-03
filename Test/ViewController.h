14b2858f40d0982d83fa5c32a5a938704a67c0a4
ericmao <ericmao@tencent.com>
2020-11-03 11:34:36 +0800

    Initial Commit

---
+++ Test/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Ericmao on 2020/11/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by Ericmao on 2020/11/3.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
